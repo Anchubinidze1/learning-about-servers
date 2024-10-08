46b1737f550d20b25084bd8aef1c11b79b83a368
Anri Chubinidze <anrichubinidze204@gmail.com>
2024-10-08 15:22:02 +0400

    Addint new repository

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+public static void main()
+{
+    String message  = "bug is fixed"
+    System.out.println("Hello World" + message);
+
+    System.out.println("Please enter the number");
+    Scanner scanner = new Scanner();
+
+    
+}
